2b3a49596fbd7dd66f4e7e36e65f67b7be279b65
DaG-del <64450307+DaG-del@users.noreply.github.com>
2026-05-02 11:13:30 +0000

    updated GForm connection

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template
+import requests
+import csv
+import io
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    records = []
+    records = get_records()
     return render_template("home.html", records=records)
+
+def get_records():
+
+    f = open("CSV.const", "r")
+
+    URL = f.read()
+
+    response = requests.get(URL)
+    response.raise_for_status()
+    
+    content = io.StringIO(response.text)
+    reader = csv.reader(content)
+
+    return list(reader)
